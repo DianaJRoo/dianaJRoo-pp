35b63d9eafadbbab43e9cbce8ee838127d7eb2b1
Diana Jimenez Roo <149723555+DianaJRoo@users.noreply.github.com>
2024-08-06 13:52:41 +0000

    avance

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -36,65 +36,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 			},
 			
-			currentUser: async (token) => {
-				try {
-					const res = await fetch(process.env.BACKEND_URL + "/api/currentUser", {
-						method: "GET",
-
-						headers: {
-							"Access-Control-Allow-Credentials": true,
-							"Authorization": 'Bearer ' + token
-						}
-					});
-					const data = await res.json();
-					if (!res.ok) throw new Error("Invalid credentials");
-
-					return data;
-				} catch (error) {
-					console.error("Error logging in:", error);
-					return false;
-				}
-			},
-			updateUser: async (token, user,) => {
-				try {
-
-					const res = await fetch(process.env.BACKEND_URL + "/api/user", {
-						method: "PUT",
-						body: JSON.stringify({
-							username: user.username,
-							first_name: user.first_name,
-							password: user.password,
-
-
-						}),
-
-						headers: {
-							"Access-Control-Allow-Credentials": true,
-							"Authorization": 'Bearer ' + token,
-							"Content-Type": "application/json"
-						}
-					});
-
-
-					if (res.status === 409) {
-						console.error("El usuario ya existe");
-						//return false; 
-					}
-					const data = await res.json();
-					if (!res.ok){
-						throw new Error(data.msg)
-					}  
-					return true; 
-				} catch (error) {
-					console.log(error.message)
-					if (error.message == "Username already exists" || error.message == "Email already exists"){
-						throw new Error(error.message); 
-					}
-					console.log(error.message)
-					console.error("Error al crear un nuevo usuario:", error);
-					return false; 
-				}
-			},
+	
+			
 
 			login: async (userInfo) => {
 				try {
