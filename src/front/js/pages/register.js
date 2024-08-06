0356c8a0e6ae0fee793ab32bedce10279ae96970
Diana Jimenez Roo <149723555+DianaJRoo@users.noreply.github.com>
2024-08-06 02:41:29 +0000

    adelanto

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -11,152 +11,143 @@ export const Register = () => {
     surName: "",
     userName: "",
     password: "",
-
-  })
+  });
 
   const handleChange = ({ target }) => {
     const { name, value } = target;
     setUserInfo({ ...userInfo, [name]: value });
-  
+  };
 
-  }
-  console.log(userInfo)
+  console.log(userInfo);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(userInfo)
-    console.log(userInfo.userName)
-    
-  
-  var registered = null
-    try{registered = await actions.createNewUser(userInfo) } 
-    catch (error) {
-     
-      console.log(error.message)
-    	if (error.message == "Username already exists"){
-       
-        alert("username already exists" );
+    console.log(userInfo);
+
+    let registered = null;
+    try {
+      registered = await actions.createNewUser(userInfo);
+    } catch (error) {
+      console.log(error.message);
+      if (error.message === "Username already exists") {
+        alert("Username already exists");
         return;
       }
-     
-    return false; 
-  }
+      alert("An error occurred. Please try again.");
+      return false;
+    }
 
-    if (registered) navigate('/login')
-    else undefined
+    if (registered) {
+      navigate('/login');
+    }
   };
 
-
-  return (<>
-  
-    <div id='register-body' className='flex flex-col justify-center mt-0 items-center bg-neutral-100' style={{backgroundImage: 'url("https://wallpapercave.com/wp/wp5539841.jpg")', backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh'}} >
-      
-      <form onSubmit={handleSubmit} className="flex flex-col items-center p-4 mx-auto max-w-screen-sm h-screen ">
-       
+  return (
+    <div
+      id='register-body'
+      className='flex flex-col justify-center mt-0 items-center bg-neutral-100'
+      style={{
+        backgroundImage: 'url("https://wallpapercave.com/wp/wp5539841.jpg")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh'
+      }}
+    >
+      <form onSubmit={handleSubmit} className="flex flex-col items-center p-4 mx-auto max-w-screen-sm h-screen">
         <div id="form1" className="border border-gray-900/10 pb-15 p-4 mx-auto max-w-screen-sm rounded-lg shadow-2">
           <h2 className="text-base font-semibold leading-7 text-gray-900">
-             Personal Information   
+            Personal Information
           </h2>
 
           <div className="sm:col-span-3 mt-2">
-              <label htmlFor="nombre" className="block text-sm font-medium leading-6 text-gray-900">
-                Name
-             </label>
-              <div className="sm:col-span-3">
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  autoComplete="name"
-                  required
-                  onChange={handleChange}
-                  value={userInfo.name}
-                  className="shadow-md block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+              Name
+            </label>
+            <div className="sm:col-span-3">
+              <input
+                id="name"
+                name="name"
+                type="text"
+                autoComplete="name"
+                required
+                onChange={handleChange}
+                value={userInfo.name}
+                className="shadow-md block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
             </div>
+          </div>
 
-            <div className="sm:col-span-3 mt-2">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Surname
-             </label>
-              <div className="sm:col-span-3">
-                <input
-                  id="surName"
-                  name="surName"
-                  type="text"
-                  autoComplete="surName"
-                  required
-                  onChange={handleChange}
-                  value={userInfo.surName}
-                  className="shadow-md block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+          <div className="sm:col-span-3 mt-2">
+            <label htmlFor="surName" className="block text-sm font-medium leading-6 text-gray-900">
+              Surname
+            </label>
+            <div className="sm:col-span-3">
+              <input
+                id="surName"
+                name="surName"
+                type="text"
+                autoComplete="surName"
+                required
+                onChange={handleChange}
+                value={userInfo.surName}
+                className="shadow-md block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
             </div>
+          </div>
 
-            <div className="sm:col-span-3 mt-2">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Username
-             </label>
-              <div className="sm:col-span-3">
-                <input
-                  id="userName"
-                  name="userName"
-                  type="text"
-                  autoComplete="userName"
-                  required
-                  onChange={handleChange}
-                  value={userInfo.userName}
-                  className="shadow-md block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+          <div className="sm:col-span-3 mt-2">
+            <label htmlFor="userName" className="block text-sm font-medium leading-6 text-gray-900">
+              Username
+            </label>
+            <div className="sm:col-span-3">
+              <input
+                id="userName"
+                name="userName"
+                type="text"
+                autoComplete="userName"
+                required
+                onChange={handleChange}
+                value={userInfo.userName}
+                className="shadow-md block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
             </div>
+          </div>
 
-            <div>
-              <div className="flex items-center justify-between ">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password                
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  onChange={handleChange}
-                  required
-                  value={userInfo.password}
-                  className="shadow-md block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-
-            <div className="footer" style={{ display: 'flex', justifyContent: 'center' }}>
-
-              <button className="flex justify-center mt-7 mb-1 bg-pink-900 hover:bg-pink-700 text-pink-200 hover:text-pink-50 font-overpass text-base font-semibold py-3 px-10 rounded "
-                type="submit"
-                title="Ingresar"
-                name="Ingresar">
-               
-               
-                  Register
-                
-              </button>
-
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
             </div>
-          </div >
-        </div >
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                onChange={handleChange}
+                required
+                value={userInfo.password}
+                className="shadow-md block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
 
-        <div className="flex justify-between items-center text-white">
-                    <Link to="/"><strong>Go Back!</strong></Link>
+          <div className="footer" style={{ display: 'flex', justifyContent: 'center' }}>
+            <button
+              className="flex justify-center mt-7 mb-1 bg-pink-900 hover:bg-pink-700 text-pink-200 hover:text-pink-50 font-overpass text-base font-semibold py-3 px-10 rounded"
+              type="submit"
+              title="Register"
+            >
+              Register
+            </button>
+          </div>
+        </div>
 
-                </div>
-
-      </form >
-      
-
-
-    </div >
-  </>
+        <div className="flex justify-between items-center text-white mt-4">
+          <Link to="/"><strong>Go Back!</strong></Link>
+        </div>
+      </form>
+    </div>
   );
-}
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -22,7 +22,21 @@ const getState = ({ getStore, getActions, setStore }) => {
 				getActions().changeColor(0, "green");
 			},
 
+			getMessage: async () => {
+				try{
+					// fetching data from the backend
+					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+					const data = await resp.json()
+					setStore({ message: data.message })
+					// don't forget to return something, that is how the async resolves
+					return data;
+				}catch(error){
+					console.log("Error loading message from backend", error)
+				}
+			},
+			
 			currentUser: async (token) => {
+
 				try {
 					const res = await fetch(process.env.BACKEND_URL + "/api/currentUser", {
 						method: "GET",
@@ -41,10 +55,72 @@ const getState = ({ getStore, getActions, setStore }) => {
 					return false;
 				}
 			},
+			updateUser: async (token, user,) => {
+				try {
 
+					const res = await fetch(process.env.BACKEND_URL + "/api/user", {
+						method: "PUT",
+						body: JSON.stringify({
+							username: user.username,
+							first_name: user.first_name,
+							password: user.password,
+
+
+						}),
+
+						headers: {
+							"Access-Control-Allow-Credentials": true,
+							"Authorization": 'Bearer ' + token,
+							"Content-Type": "application/json"
+						}
+					});
+
+
+					if (res.status === 409) {
+						console.error("El usuario ya existe");
+						//return false; 
+					}
+					const data = await res.json();
+					if (!res.ok){
+						throw new Error(data.msg)
+					}  
+					return true; 
+				} catch (error) {
+					console.log(error.message)
+					if (error.message == "Username already exists" || error.message == "Email already exists"){
+						throw new Error(error.message); 
+					}
+					console.log(error.message)
+					console.error("Error al crear un nuevo usuario:", error);
+					return false; 
+				}
+			},
+
+			login: async (userInfo) => {
+				try {
+					const res = await fetch(process.env.BACKEND_URL + "/api/login", {
+						method: "POST",
+						body: JSON.stringify(userInfo),
+						headers: {
+							"Content-Type": "application/json"
+						}
+					});
+					const data = await res.json();
+					if (!res.ok) throw new Error("Invalid credentials");
+
+					// Guardar el token en sessionStorage
+					sessionStorage.setItem("userData", JSON.stringify(data));
+
+					setStore({ userToken: data });
+					return true;
+				} catch (error) {
+					console.error("Error logging in:", error);
+					return false;
+				}
+			},
 			createNewUser: async (userInfo) => {
 				try {
-					const res = await fetch(process.env.BACKEND_URL + '/api/register', {
+					const res = await fetch(`${process.env.BACKEND_URL}/api/register`, {
 						method: "POST",
 						body: JSON.stringify(userInfo),
 						headers: {
@@ -62,7 +138,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					return true; 
 				} catch (error) {
 					console.log(error.message)
-					if (error.message == "Username already exists"){
+					if (error.message == "Username already exists" ){
 						throw new Error(error.message); 
 					}
 					console.log(error.message)
@@ -70,30 +146,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					return false; 
 				}
 			},
-	
-			login: async (userInfo) => {
-				try {
-					const res = await fetch(process.env.BACKEND_URL + "/api/login", {
-						method: "POST",
-						body: JSON.stringify(userInfo),
-						headers: {
-							"Content-Type": "application/json"
-						}
-					});
-					const data = await res.json();
-					if (!res.ok) throw new Error("Invalid credentials");
-	
-					// Guardar el token en sessionStorage
-					sessionStorage.setItem("userData", JSON.stringify(data));
-	
-					setStore({ userToken: data });
-					return true;
-				} catch (error) {
-					console.error("Error logging in:", error);
-					return false;
-				}
-			},
-	
+
 			logOut: () => {
 				console.log('out')
 				sessionStorage.clear()
@@ -102,43 +155,20 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			},
-
-			
 
 
-		},
-
-		
-
-
-
+		}
 	};
+
+
+
+
+
+
 };
+
+
+
+
 
 export default getState;
